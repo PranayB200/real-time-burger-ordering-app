a016fcf3b7ce10a4a458973139bf7902c9539f77
Pranay Bansal <44213415+PranayB200@users.noreply.github.com>
2021-03-30 02:22:17 +0530

    auth system

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 
 const PORT = process.env.PORT || 3000
 
@@ -43,6 +44,12 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hours
 }))
 
+// Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash())
 
 app.use(express.static('public'))
@@ -50,6 +57,7 @@ app.use(express.static('public'))
 // Global Middlewares
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
