29d1661a457891d487a9e3934bc68d19f14a5c80
Pranay Bansal <44213415+PranayB200@users.noreply.github.com>
2021-04-01 22:07:06 +0530

    Some final bug fixes.

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
-
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
-const expressLayout = require('express-ejs-layouts')
 const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+const PORT = process.env.PORT || 3300
 const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
@@ -12,91 +12,73 @@ const MongoDbStore = require('connect-mongo')(session)
 const passport = require('passport')
 const Emitter = require('events')
 
-const PORT = process.env.PORT || 3000
-
-// const dbUrl = 'mongodb://localhost/pizza'
-const dbUrl = process.env.DB_URL 
-
-
-app.use(express.json());
-app.use(express.urlencoded({extended:false}))
-
-// Dababase conection
-mongoose.connect(dbUrl,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-})
+// Database connection
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log('Database connected..');
+    console.log('Database connected...');
 }).catch(err => {
-    console.log('Connection failed..');
+    console.log('Connection failed...')
 });
 
 
 // Session store
-let mongoStore=new MongoDbStore({
-    url:dbUrl,
-    mongooseConnection:connection,
-    touchAfter:24*60*60,
-    collection:'sessions',
-    secret: process.env.COOKIE_SECRET  
-
-})
+let mongoStore = new MongoDbStore({
+                mongooseConnection: connection,
+                collection: 'sessions'
+            })
 
 // Event emitter
 const eventEmitter = new Emitter()
 app.set('eventEmitter', eventEmitter)
 
-
 // Session config
 app.use(session({
-    secret: process.env.COOKIE_SECRET, // Check .env in the root folder.
+    secret: process.env.COOKIE_SECRET,
     resave: false,
-    store: mongoStore, // to store the sessions in the 'mongoStore'
+    store: mongoStore,
     saveUninitialized: false,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hours
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
 }))
 
 // Passport config
 const passportInit = require('./app/config/passport')
 passportInit(passport)
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(flash())
-
+// Assets
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-// Global Middlewares
+// Global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
     res.locals.user = req.user
     next()
 })
-
 // set Template engine
 app.use(expressLayout)
-
-app.set('views', path.join(__dirname + '/resources/views'))
+app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-
-// ALL OF THE ROUTES SHOULD COME AFTER SETTING THE VIEW AND LAYOUT. ELSE, THE LAYOUT WON'T WORK.
-require('./routes/web.js')(app);
-
-// Run using: yarn dev
-const server = app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`)
+require('./routes/web')(app)
+app.use((req, res) => {
+    res.status(404).render('errors/404')
 })
 
+const server = app.listen(PORT , () => {
+            console.log(`Listening on port ${PORT}`)
+        })
+
 // Socket
+
 const io = require('socket.io')(server)
 io.on('connection', (socket) => {
       // Join
-      socket.on('join', (orderId) => { // Creating private socket rooms for each order ID.
+      socket.on('join', (orderId) => {
         socket.join(orderId)
       })
 })
