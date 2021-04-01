3c5414e7896f8595016d78e003464fc3e053557c
Pranay Bansal <44213415+PranayB200@users.noreply.github.com>
2021-04-01 23:27:49 +0530

    bug fixes

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useCreateIndex:tru
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('Database connected...');
-}).catch(err => {
-    console.log('Connection failed...')
 });
 
 
